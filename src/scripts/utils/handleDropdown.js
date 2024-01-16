491419fa61e725011cd544bc29c2a94378892c80
rockethelll <51137055+rockethelll@users.noreply.github.com>
2024-01-16 11:03:36 +0100

    minify all files

--- src/scripts/utils/handleDropdown.js
+++ src/scripts/utils/handleDropdown.js
@@ -1,4 +1,4 @@
-import { capitalizeFirstLetter } from "./capitalizeFirstLetter.js";
+import { capitalizeFirstLetter } from "./capitalizeFirstLetter.min.js";
 
 // Function to update dropdown lists based on the recipes that matched the search criteria
 export function updateListOptions(matchedRecipes) {
--- src/scripts/utils/searchTag.js
+++ src/scripts/utils/searchTag.js
@@ -1,5 +1,5 @@
-import { recipes } from "../../data/recipes.js";
-import { applyFilters } from "./mainFilter.js";
+import { recipes } from "../../data/recipes.min.js";
+import { applyFilters } from "./mainFilter.min.js";
 
 export let filteredRecipesState = {
   filteredRecipesByTags: [],
